d09a8fb39928ad29d740490e017f5b137a5731d1
Yi-Han Chen <hannah51125@gmail.com>
2023-09-23 10:36:04 -0400

    feat: Add append function in FreeList

--- src/FreeList.java
+++ src/FreeList.java
@@ -1,25 +1,47 @@
+class Node {
+    int val;
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    Node next;
+}
+
 public class FreeList {
     Node head;
+    Node tail;
 
     public Node getHead() {
         return head;
     }
 
-    class Node {
-        int val;
 
-        public Node(int val) {
-            this.val = val;
-        }
+    public FreeList() {
+        this.head = null;
+        this.tail = null;
     }
 
     public FreeList(int headVal) {
         this.head = new Node(headVal);
+        this.tail = this.head;
     }
 
-    //get Nth Node in free list, return the position of node
+    //get Nth Node in free list, return the value of node
     public long getNthNode(int nth) {
-        return head.val;
+        int i = 0;
+        Node cur = head;
+        while (i < nth) {
+            cur = cur.next;
+            i++;
+        }
+        return cur.val;
+    }
+
+    //append a node to the end of free list
+    public void append(int val) {
+        tail.next = new Node(val);
+        tail = tail.next;
     }
 
 }
--- test/TestFreeList.java
+++ test/TestFreeList.java
@@ -1,14 +1,30 @@
 import org.junit.Test;
 
+
 public class TestFreeList {
 
     @Test
+    public void testInitFreeList() {
+        FreeList fl = new FreeList();
+        assert fl.getHead() == null;
+    }
+
+    @Test
     public void testGetFirstItemInFreeList() {
-        FreeList fl = new FreeList(-1);
+        FreeList fl = new FreeList(2);
         assert fl.getHead() != null;
 
         long rtn = fl.getNthNode(0);
 
         assert rtn == fl.getHead().val;
     }
+
+    @Test
+    public void testAppendItemInFreeList() {
+        FreeList fl = new FreeList(2);
+
+        fl.append(3);
+        assert fl.getNthNode(1) == 3;
+    }
+
 }
