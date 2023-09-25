694da01e0d59f5101db9f27f71de3c05edd285d5
Chi Chen <stephchen55@gmail.com>
2023-09-25 18:44:22 -0400

    add BNode class properties

--- src/BNode.java
+++ src/BNode.java
@@ -1,2 +1,12 @@
+
+//BNode types
+public const int internalNode = 1;
+public const inr leafNode = 2;
+
 public class BNode {
+    private int nodeType;
+    private int keyNums;
+    private List<BNode> children;
+    private List<Integer> offsets;
+    private byte[] keyValues; //not sure what to do with the KV pairs yet
 }
