37392080757b7de0f1b14240c41d1943921bebcc
Yi-Han Chen <hannah51125@gmail.com>
2023-09-26 10:05:31 -0400

    refactor: Use generics

--- test/TestFreeList.java
+++ test/TestFreeList.java
@@ -8,31 +8,41 @@ public class TestFreeList {
 
     @Test
     public void testInitFreeList() {
-        FreeList fl = new FreeList();
+        FreeList<Object> fl = new FreeList<>();
         assertNull(fl.getHead());
     }
 
     @Test
     public void testGetFirstItemInFreeList() {
-        FreeList fl = new FreeList(2);
+        FreeList<Integer> fl = new FreeList<>(2);
         assert fl.getHead() != null;
 
-        long rtn = fl.getNthNode(0);
+        Node rtn = fl.getNthNode(0);
 
-        assertEquals(rtn, fl.getHead().val);
+        assertEquals(rtn.val, fl.getHead().val);
+    }
+
+    @Test
+    public void testGetSecondItemInFreeList() {
+        FreeList<Integer> fl = new FreeList<>(2);
+        fl.append(3);
+        assertEquals(3, fl.getNthNode(1).val);
+
+        fl.append(5);
+        assertEquals(5, fl.getNthNode(2).val);
     }
 
     @Test
     public void testAppendItemInFreeList() {
-        FreeList fl = new FreeList(2);
+        FreeList<Integer> fl = new FreeList<>(2);
 
         fl.append(3);
-        assertEquals(3, fl.getNthNode(1));
+        assertEquals(3, fl.getNthNode(1).val);
     }
 
     @Test
     public void testFreeListTotal() {
-        FreeList fl = new FreeList();
+        FreeList<Integer> fl = new FreeList<>();
         fl.append(1);
         fl.append(2);
         assertEquals(2, fl.total());
@@ -40,13 +50,13 @@ public class TestFreeList {
 
     @Test
     public void testFreeListTotal_isEmpty() {
-        FreeList fl = new FreeList();
+        FreeList<Integer> fl = new FreeList<>();
         assertEquals(0, fl.total());
     }
 
     @Test
     public void testFreeListTotal_isOne_if_head_is_initialized() {
-        FreeList fl = new FreeList(2);
+        FreeList<Integer> fl = new FreeList<>(2);
         assertEquals(1, fl.total());
     }
 }
